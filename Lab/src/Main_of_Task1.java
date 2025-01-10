fe28ea7ac01ccc7e25ba2cd98f13b47f4f48ec74
Oakiez <78066010+Duckyz4826@users.noreply.github.com>
2025-01-10 18:16:04 +0700

    Updated Main_of_Task1.java

--- Lab/src/Main_of_Task1.java
+++ Lab/src/Main_of_Task1.java
@@ -1,13 +1,14 @@
 public class Main2 {
     public static void main(String[] args){
-        Parent myParent = new Parent("673380425-2","Mommy","Khon Kaen");
-        Child myChild = new Child("673380425-2","Mommy",
+        Parent myParent = new Parent("000000","Mommy","Khon Kaen");
+        Child myChild = new Child("673380425-2","Wongsathon Thanayod",
                                 "Khon Kaen","Oak","KKU");
         
-        System.out.println();
+        System.out.println("===== Parent =====");
         myParent.showPersonalInfo();
-
+        
         System.out.println();
+        System.out.println("===== Child =====");
         myChild.showPersonalInfo();
 
     }
